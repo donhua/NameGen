825a672215c86bf7d0a79f60e739e868d513421d
Alexei <suvorov.mo@yandex.ru>
2022-01-16 10:02:44 +0300

    1

---
+++ NameGen/GenMix.cs
@@ -0,0 +1,28 @@
+﻿namespace NameGen
+{
+    class GenMix
+    {
+        /* из каждого массива берет по 1 рандомному элементу
+        и составляет из них слово */
+
+        private string[] a;
+        private string[] b;
+        Random r = new Random();
+        public GenMix(string[] a, string[] b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        private string RndPart(string[] name)
+        {
+            int i = r.Next(name.Length);
+            return name[i];
+        }
+        public string Mix()
+        {
+            return RndPart(a) + RndPart(b);
+        }
+
+    }
+
+}
