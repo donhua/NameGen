a18401d3859680eed06bcf5563d47b4b1d710854
Alexei <suvorov.mo@yandex.ru>
2022-01-16 11:09:48 +0300

    1

--- NameGen/GenMix.cs
+++ NameGen/GenMix.cs
@@ -4,25 +4,16 @@
     {
         /* из каждого массива берет по 1 рандомному элементу
         и составляет из них слово */
-
-        private string[] a;
-        private string[] b;
-        public GenMix(string[] a, string[] b)
-        {
-            this.a = a;
-            this.b = b;
-        }
-        private string RndPart(string[] name)
+        public string RndPart(string[] name)
         {
             Random r = new Random();
             int i = r.Next(name.Length);
             return name[i];
         }
-        public string Mix()
+        public string Mix2(string[] a, string[] b)
         {
             return RndPart(a) + RndPart(b);
         }
-
     }
 
 }
---
+++ NameGen/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace NameGen
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] a = {"Sed", "Gart", "Akin" };
+            string[] b = {"aze", "ecr", "yrd" };   
+            GenMix GM = new();
+            Console.WriteLine(GM.Mix2(a, b));
+            Console.WriteLine(GM.RndPart(a));
+            Console.ReadLine();
+        }
+    }
+}
